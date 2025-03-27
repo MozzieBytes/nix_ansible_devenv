3eb428bb44f6111a56ae2984e4b59a16cb13d156
Mozzie <15324906+MozzieBytes@users.noreply.github.com>
2025-03-27 00:13:02 +0000

    Chore: Nix format after configuring flake

--- flake.nix
+++ flake.nix
@@ -5,26 +5,41 @@
     flake-utils.url = "github:numtide/flake-utils";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
-  outputs = { self, nixpkgs, flake-utils, treefmt-nix, ... }: 
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-      treefmtPrograms = builtins.attrValues treefmtEval.config.build.programs;
-    in {
-      formatter = treefmtEval.config.build.wrapper;
-      checks.formatting = treefmtEval.config.build.check self;
-      devShells.default = pkgs.mkShell { buildInputs = with pkgs; [
-          git
-          nil
-          ansible
-          ansible-lint
-          openssh
-          python3
-          python3Packages.jmespath
-        ] ++ treefmtPrograms;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
         };
-    };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+        treefmtPrograms = builtins.attrValues treefmtEval.config.build.programs;
+      in
+      {
+        formatter = treefmtEval.config.build.wrapper;
+        checks.formatting = treefmtEval.config.build.check self;
+        devShells.default = pkgs.mkShell {
+          buildInputs =
+            with pkgs;
+            [
+              git
+              nil
+              ansible
+              ansible-lint
+              openssh
+              python3
+              python3Packages.jmespath
+            ]
+            ++ treefmtPrograms;
+        };
+      }
+    );
 }
--- treefmt.nix
+++ treefmt.nix
@@ -1,5 +1,7 @@
 {
   projectRootFile = "flake.nix";
+
   programs.nixfmt.enable = true;
   programs.yamlfmt.enable = true;
+  settings.global.excludes = [ ".envrc" ];
 }
