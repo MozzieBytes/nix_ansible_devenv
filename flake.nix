b4368763fd8cd64da08dc903b82b20243ced708d
Mozzie <15324906+MozzieBytes@users.noreply.github.com>
2025-03-27 00:13:02 +0000

    Feat: Adding nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Demo nix dev-container";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+  outputs = { self, nixpkgs, flake-utils, treefmt-nix, ... }: 
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      treefmtPrograms = builtins.attrValues treefmtEval.config.build.programs;
+    in {
+      formatter = treefmtEval.config.build.wrapper;
+      checks.formatting = treefmtEval.config.build.check self;
+      devShells.default = pkgs.mkShell { buildInputs = with pkgs; [
+          git
+          nil
+          ansible
+          ansible-lint
+          openssh
+          python3
+          python3Packages.jmespath
+        ] ++ treefmtPrograms;
+        };
+    };
+}
